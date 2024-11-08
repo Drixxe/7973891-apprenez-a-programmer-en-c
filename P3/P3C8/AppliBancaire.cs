b5075398d5b43d54e4fa2b46706312138ef992f0
Drixxe <drixe82@gmail.com>
2024-11-08 14:47:39 +0100

    Create AppliBancaire.cs

---
+++ P3/P3C8/AppliBancaire.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3C8
+{
+    internal class AppliBancaire
+    {
+    }
+}
